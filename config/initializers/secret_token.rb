ef54c102c33ea8dca4b1de8a5784667ef0b93692
edge-evolution <eflores2@gmail.com>
2013-10-08 21:49:24 +0000

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monkey::Application.config.secret_token = '25922f83d58df0dc9f6d8f4a453636649399c5f1caf20f00a804622318d58b4585380aa93dd14aa1ad8d83ff14a27531d12fc3cf40651143d7f6b377f56f7961'
